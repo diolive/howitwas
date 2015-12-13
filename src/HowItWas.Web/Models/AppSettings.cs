e2ed49e5441ec8c05d30215fa5c1092d1e375709
DioLive Studio <dio@live.ru>
2015-12-13 14:48:38 +0300

    Added AppSettings wrapper

---
+++ src/HowItWas.Web/Models/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace HowItWas.Web.Models
+{
+    public class AppSettings
+    {
+        public string SiteTitle { get; set; }
+
+        public string AppName { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Owner { get; set; }
+
+        public string Version { get; set; }
+
+        public string GitHash { get; set; }
+    }
+}
