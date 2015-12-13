304503fbced6db2bb53daffb6f583aa4a28dec83
DioLive Studio <dio@live.ru>
2015-12-13 10:29:22 +0300

    New entity: Survey

--- src/HowItWas.Web/Migrations/20151213001640_Survey.Designer.cs
+++ src/HowItWas.Web/Migrations/20151213001640_Survey.Designer.cs
@@ -8,9 +8,10 @@ using HowItWas.Web.Models;
 namespace HowItWas.Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20151213001640_Survey")]
+    partial class Survey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
---
+++ src/HowItWas.Web/Models/Survey.cs
@@ -0,0 +1,20 @@
+﻿namespace HowItWas.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Survey
+    {
+        [Required, StringLength(36)]
+        public string Id { get; set; } = Utils.NewID;
+
+        [Required]
+        public string Description { get; set; }
+
+        public bool Closed { get; set; }
+
+        public ApplicationUser Owner { get; set; }
+
+        [Required]
+        public string Owner_Id { get; set; }
+    }
+}
