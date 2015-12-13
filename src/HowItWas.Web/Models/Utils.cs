7cc7132aa4a0a7d006c5ee8a0e143d16ad861ef0
DioLive Studio <dio@live.ru>
2015-12-13 10:29:00 +0300

    Added Utils

---
+++ src/HowItWas.Web/Models/Utils.cs
@@ -0,0 +1,11 @@
+﻿namespace HowItWas.Web.Models
+{
+    using System;
+
+    public class Utils
+    {
+        public static Random Random { get; } = new Random();
+
+        public static string NewID => Guid.NewGuid().ToString();
+    }
+}
